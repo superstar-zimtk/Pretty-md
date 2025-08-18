f3761e0be89f0889c50356006525573a035bd829
superstar-zimtk <takuemsax@gmail.com>
2025-08-18 10:02:37 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'pretty 𝐌ᴅ',
+  author: 'Superstar,
+  botName: "pretty 𝐌ᴅ",
+  botOwner: 'Superstar, // Your name
+  ownerNumber: '254798570132', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.2.4",
+};
+
+module.exports = settings;
