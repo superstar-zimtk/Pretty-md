8c54a4651a593b81032b2529411d3af09220be23
superstar-zimtk <takuemsax@gmail.com>
2025-08-29 00:02:13 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Superstar,
   botName: "pretty 𝐌ᴅ",
   botOwner: 'Superstar, // Your name
-  ownerNumber: '254798570132', //Set your number here without + symbol.
+  ownerNumber: '263715305976', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
