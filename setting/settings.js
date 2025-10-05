7eea04006e0b3c4c8a90bad9de479719a9de969f
superstar-zimtk <takuemsax@gmail.com>
2025-10-05 15:40:03 +0200

    Update settings.js

--- setting/settings.js
+++ setting/settings.js
@@ -8,28 +8,28 @@ global.SESSION_ID = process.env.SESSION_ID || ''
 global.owner = "263715305976"
 global.developer = "263715305976"
 global.bot = ""
-global.devname = "superstar"
-global.ownername = process.env.OWNER_NAME ||'superstar'
-global.botname = "pretty Md"
+global.devname = "Superstar"
+global.ownername = process.env.OWNER_NAME ||'Superstar'
+global.botname = "Pretty Md"
 global.versisc = "2"
 global.packname = "⎋Pretty Md"
 //~~~~~~~~~~~ Settings Sosmed ~~~~~~~~~~~//
-global.linkwa = "https://wa.me/263715305976"
-global.linkyt = "https://www.youtube.com/superstar"
+global.linkwa = "https://wa.me/superstars"
+global.linkyt = "https://www.youtube.com/superstars"
 global.linktt = "https://tiktok.com"
 global.linktele = "https://t.me"
 
 //~~~~~~~~~~~ Settings Bot ~~~~~~~~~~~//
 global.prefix = process.env.BOT_PREFIX ||'.'
 global.autoRecording = false
-global.autoTyping = true
+global.autoTyping = false
 global.autorecordtype = false
 global.autoread = process.env.AUTO_READ || false
 global.autobio = false
 global.anti92 = false
 global.owneroff = false
 global.statusview = process.env.AUTO_STATUS || true
-
+global.autoreact = process.env.AUTO_REACT || 'false'
 //~~~~~~~~~~~ Settings Thumbnail ~~~~~~~~~~~//
 global.thumbbot = "https://url.bwmxmd.online/Adams.poh4tuhs.jpg"
 global.thumbown = "https://url.bwmxmd.online/Adams.poh4tuhs.jpg"
@@ -41,15 +41,15 @@ global.channel = "https://whatsapp.com/channel/0029Vb9qprVJuyAJxcTO252t"
 
 //~~~~~~~~~~~ Settings Message ~~~~~~~~~~~//
 global.mess = {
-  developer: " `[ Developer Only!! ]` \n This feature is for developers only!!",
-  owner: " `[ Owner Only!! ]` \n This feature is for owners only!!",
-  group: " `[ Group Only!! ]` \n This feature is for group chats only!!",
-  private: " `[ Private Only!! ]` \n This feature is for private chats only!!",
-  admin: " `[ Admin Only!! ]` \n This feature is for admins only!!",
-  botadmin: " `[ Bot Admin Only!! ]` \n This feature is for bot admins only!!",
-  wait: " `[ Wait!! ]` \n Please wait, loading...",
-  error: " `[ Error!! ]` \n An error occurred!!",
-  done: " `[ Done!! ]` \n Process completed!!"
+  developer: "This feature is for developers only!!",
+  owner: " feature is for owners only!",
+  group: "This feature is for group chats only!!",
+  private: "This feature is for private chats only!",
+  admin: "This feature is for admins only!!",
+  botadmin: "This feature is for bot admins only!!",
+  wait: "Please wait, loading...",
+  error: "An error occurred!",
+  done: "Process completed!"
 }
 
 let file = require.resolve(__filename)
